98f508752b0a258142ca354a7bb945c56aafc4a2
hamza-nejib <hamza-nejib@outlook.com>
2024-10-31 14:53:23 +0100

    Transformation de la maquette en HTML et CSS page 2

--- src/components/ToDoForm/ToDoForm.jsx
+++ src/components/ToDoForm/ToDoForm.jsx
@@ -2,17 +2,63 @@ import React from 'react';
 import './ToDoForm.css';
 
 function ToDoForm() {
-    return (
-      <div className='wrapper2'>
+  return (
+    <div className='wrapper2'>
       <div className="frame2">
-        <h2>React To-Do List</h2>
-        <div class="sous-frame2">
-
-        </div>
-        <p className='copyright'>Patryk Kielian © 2023 all rights reserved</p>
+        <h2 className='h2-2'>React To-Do List</h2>
+        <form className="sous-frame2">
+          <h3>Add a new to-do:</h3>
+          <div className="form-columns">
+            <div className="left-column">
+              <div className="form-group">
+                <label htmlFor="name">Name:</label>
+                <input type="text" id="name" placeholder="name for the task you're going to do" />
+              </div>
+              <div className="form-group">
+                <label htmlFor="description">Description:</label>
+                <textarea id="description" placeholder="a short description of the task - can be omitted"></textarea>
+              </div>
+              <div className="form-group">
+                <label htmlFor="category">Category:</label>
+                <input type="text" id="category" placeholder="e.g. household, school, work" />
+              </div>
+              <div className="form-group">
+                <label htmlFor="date">Date:</label>
+                <input type="text" id="date" placeholder="dd/mm/yyyy - can be ommitted" />
+              </div>
+              <div className="form-group">
+                <label htmlFor="time">Time:</label>
+                <input type="text" id="time" placeholder="hh:mm - can be ommitted" />
+              </div>
+            </div>
+            
+            <div className="right-column">
+              <div className="form-group">
+                <label htmlFor="priority">Priority:</label>
+                <select id="priority">
+                  <option>select from dropdown</option>
+                </select>
+              </div>
+              <div className="form-group">
+                <label htmlFor="fulfillment">Fulfillment:</label>
+                <input type="range" id="fulfillment" />
+              </div>
+              <div className="button-group">
+                <button type="button" className="btn-save"><span className='txt-save'>Save</span></button>
+                <button type="button" className="btn-cancel"><span className='txt-cancel'>Cancel</span></button>
+              </div>
+            </div>
+          </div>
+          
+    
+        </form>
+        <footer>
+          <p className='copyright2'>Patryk Kielian © 2023 all rights reserved</p>
+        </footer>
+        
       </div>
     </div>
-    );
-  }
+  );
+}
 
 export default ToDoForm;
--- src/components/ToDoList/ToDoList.jsx
+++ src/components/ToDoList/ToDoList.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import './ToDoList.css';
+import modifIcon from '../../images/modif.png';
+import deleteIcon from '../../images/delete.png';
+import { useNavigate } from 'react-router-dom';
 
 function ToDoList() {
+    
+    const navigate = useNavigate();
+    const handleClick = () => {
+        navigate('/add');
+    };
+
   return (
     <div className='wrapper'>
       <div className="frame">
-        <h2>React To-Do List</h2>
-        <button className='btn-add'><span class="text-add">Add a new to-do</span></button>
+        <h2 className='h2-1'>React To-Do List</h2>
+        <button className='btn-add' onClick={handleClick}><span class="text-add">Add a new to-do</span></button>
         <button className='btn-all'><span class="text-all">All</span></button>
         <button className='btn-todo'><span class="text-todo">To-do</span></button>
         <button className='btn-completed'><span class="text-add">Completed</span></button>
@@ -20,37 +29,46 @@ function ToDoList() {
                     <th>When</th>
                     <th>Priority</th>
                     <th>Fulfillment</th>
+                    <th></th>
                 </tr>
                 </thead>
                 <tbody>
                 <tr>
-                    <td>Learn React</td>
-                    <td>Managing State, Escape Hatches, Effects</td>
-                    <td>Programming</td>
-                    <td>-</td>
-                    <td>High</td>
-                    <td>30%</td>
+                    <td id='tr-task'>Learn React</td>
+                    <td id='tr-desc'>Managing State, Escape Hatches, Effects</td>
+                    <td id='tr-cat'>Programming</td>
+                    <td id='tr-when'>-</td>
+                    <td id='tr-prio'>High</td>
+                    <td id='tr-ful'>30%</td>
+                    <td><a><img className='modif-del' src={modifIcon}/></a></td>
+                    <td><a><img className='modif-del' src={deleteIcon}/></a></td>
                 </tr>
                 <tr>
-                    <td>Shopping</td>
-                    <td>Potatoes, Onions, Eggs, Olive Oil</td>
+                    <td id='tr-task'>Shopping</td>
+                    <td id='tr-desc'>Potatoes, Onions, Eggs, Olive Oil</td>
                     <td>Household</td>
                     <td>26.02.2023</td>
                     <td>High</td>
                     <td>0%</td>
+                    <td><a><img className='modif-del' src={modifIcon}/></a></td>
+                    <td><a><img className='modif-del' src={deleteIcon}/></a></td>
                 </tr>
                 <tr>
-                    <td>Buy the tickets</td>
-                    <td>at cheaptickets.com/shanghai</td>
+                    <td id='tr-task'>Buy the tickets</td>
+                    <td id='tr-desc'>at cheaptickets.com/shanghai</td>
                     <td>Travel</td>
                     <td>12.01.2023 12:00</td>
                     <td>Medium</td>
                     <td>100%</td>
+                    <td><a><img className='modif-del' src={modifIcon}/></a></td>
+                    <td><a><img className='modif-del' src={deleteIcon}/></a></td>
                 </tr>
                 </tbody>
             </table>
         </div>
-        <p className='copyright'>Patryk Kielian © 2023 all rights reserved</p>
+        <footer>
+          <p className='copyright'>Patryk Kielian © 2023 all rights reserved</p>
+        </footer>
       </div>
     </div>
   );
